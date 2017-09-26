9d77c8fd34aa93b7e09693a0e2e6904e67905ab7
lzk2017 <lzk2java@126.com>
2017-09-26 20:34:07 +0800

    动态加载动态链接库（dll）http://blog.csdn.net/cruise_h/article/details/41575481

--- src/main/java/com/lzk/myCode/dm/GetMousePointWindow.java
+++ src/main/java/com/lzk/myCode/dm/GetMousePointWindow.java
@@ -1,12 +1,48 @@
 package com.lzk.myCode.dm;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+
 import com.lzk.myCode.dm.function.DM;
 
 public class GetMousePointWindow {
-
+	
 	public static void main(String[] args) {
 		DM dm = new DM();
 		Object getMousePointWindow = dm.GetMousePointWindow();
 		System.out.println(getMousePointWindow);
 	}
+	
+	static{
+		try {
+			String dllPath = System.getProperty("user.dir")+"/src/main/resources/dll";
+			addLibraryDir(dllPath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void addLibraryDir(String libraryPath) throws IOException {  
+        try {  
+            Field field = ClassLoader.class.getDeclaredField("usr_paths");
+            field.setAccessible(true);  
+            String[] paths = (String[]) field.get(null);  
+            for (int i = 0; i < paths.length; i++) {  
+                if (libraryPath.equals(paths[i])) {  
+                    return;  
+                }  
+            }  
+  
+            String[] tmp = new String[paths.length + 1];  
+            System.arraycopy(paths, 0, tmp, 0, paths.length);  
+            tmp[paths.length] = libraryPath;  
+            field.set(null, tmp);  
+        } catch (IllegalAccessException e) {  
+            throw new IOException(  
+                    "Failedto get permissions to set library path");  
+        } catch (NoSuchFieldException e) {  
+            throw new IOException(  
+                    "Failedto get field handle to set library path");  
+        }  
+    }  
 }
